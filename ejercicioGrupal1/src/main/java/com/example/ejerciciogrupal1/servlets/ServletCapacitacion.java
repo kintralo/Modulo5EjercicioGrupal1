153b6fa4ebd8b6ae3db0ff3d03736b458711d81e
Pablov <juan.vasquez@valposystem.com>
2023-07-17 20:10:25 -0400

    Creación de listas de capacitaciones

--- ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/servlets/ServletCapacitacion.java
+++ ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/servlets/ServletCapacitacion.java
@@ -1,5 +1,6 @@
 package com.example.ejerciciogrupal1.servlets;
 
+import com.example.ejerciciogrupal1.models.Capacitacion;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,16 +9,30 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
 @WebServlet(name = "servletCapacitacion", value = "/servlet-capacitacion")
 public class ServletCapacitacion extends HttpServlet {
+
+   private ArrayList<Capacitacion> listaCapacitaciones = new ArrayList<Capacitacion>();
 
     private static final long serialVersionUID = 1L;
     public ServletCapacitacion() {
     }
 
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("codigoCapacitacion", request.getParameter("txtCodigoCapacitacion"));
+        /**request.setAttribute("codigoCapacitacion", request.getParameter("txtCodigoCapacitacion"));
         request.setAttribute("Rut", request.getParameter("txtRut"));
         request.setAttribute("Dia", request.getParameter("txtDia"));
         request.setAttribute("Hora", request.getParameter("txtHora"));
@@ -27,6 +42,28 @@ public class ServletCapacitacion extends HttpServlet {
        // RequestDispatcher rd = request.getRequestDispatcher("capacitacion.jsp");
         RequestDispatcher rd = request.getRequestDispatcher("listaCapacitaciones.jsp");
         rd.forward(request,  response);
+
+        String dato = request.getParameter("dato");
+        listaDatos.add(dato);*/
+
+        int identificador = Integer.parseInt(request.getParameter("txtCodigoCapacitacion"));
+        int run = Integer.parseInt(request.getParameter("txtRut"));
+        String dia =  request.getParameter("txtDia");
+        String horaIngresada = request.getParameter("txtHora");
+        LocalTime hora = LocalTime.parse(horaIngresada);
+        String lugar = request.getParameter("txtLugarCapacitacion");
+        String duracion = request.getParameter("txtDuracion");
+        int cantAsistentes = Integer.parseInt(request.getParameter("txtAsistentes"));
+
+        Capacitacion capacitacion = new Capacitacion(identificador, run, dia, hora, lugar, duracion, cantAsistentes);
+
+        listaCapacitaciones.add(capacitacion);
+
+        request.setAttribute("listaCapacitaciones", listaCapacitaciones);
+        //RequestDispatcher rd = request.getRequestDispatcher("listaCapacitaciones.jsp");
+        //rd.forward(request,  response);// enviar la solicitud y la respuesta al archivo JSP "tabla.jsp"
+        request.getRequestDispatcher("listaCapacitaciones.jsp").forward(request, response); // enviar la solicitud y la respuesta al archivo JSP "tabla.jsp"
+
     }
 
     /**
@@ -40,4 +77,32 @@ public class ServletCapacitacion extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int identificador = Integer.parseInt(request.getParameter("txtCodigoCapacitacion"));
+
+        // obtener la lista de capacitaciones del alcance de la aplicación
+        List<Capacitacion> listaCapacitaciones = (List<Capacitacion>) getServletContext().getAttribute("listaCapacitaciones");
+
+        // eliminar la capacitación correspondiente de la lista
+        for (Capacitacion capacitacion : listaCapacitaciones) {
+            if (capacitacion.getIdentificador() == identificador ) {
+                listaCapacitaciones.remove(capacitacion);
+                break;
+            }
+        }
+
+        // redirigir a tabla.jsp para actualizar la tabla
+        response.sendRedirect(request.getContextPath() + "/listaCapacitaciones.jsp");
+    }
+
+
 }
