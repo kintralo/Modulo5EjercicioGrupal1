75f8c166ff1b0aa9d01985e1cf5aeea45015ff86
Pablov <juan.vasquez@valposystem.com>
2023-07-24 16:10:15 -0400

    Creación de paquete modelo con clases Administrativo.java, Cliente.java, Profesional.java, Usuario.java

---
+++ ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/models/Profesional.java
@@ -0,0 +1,94 @@
+package com.example.ejerciciogrupal1.models;
+
+import java.time.LocalDate;
+/**
+ * @author  Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * @version 1.0
+ */
+/**
+ * Creación Clase Profesional.
+ */
+public class Profesional extends Usuario {
+    //Definición de atributos
+    private String titulo;
+    private LocalDate fechaIngreso;
+
+    /**
+     *
+     */
+    public Profesional() {
+    }
+
+    /**
+     * @param titulo
+     * @param fechaIngreso
+     */
+    public Profesional(String titulo, LocalDate fechaIngreso) {
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * @param run
+     * @param nombre
+     * @param apellidos
+     * @param fechaNacimiento
+     * @param edad
+     * @param titulo
+     * @param fechaIngreso
+     */
+    public Profesional(int run, String nombre, String apellidos, LocalDate fechaNacimiento, Integer edad, String titulo,
+                       LocalDate fechaIngreso) {
+        super(run, nombre, apellidos, fechaNacimiento, edad);
+        this.titulo = titulo;
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * @return titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return fechaIngreso
+     */
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    /**
+     * @param fechaIngreso
+     */
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    /**
+     * @return run, nombres, fechaNacimiento, edad.
+     */
+    @Override
+    public String toString() {
+        return "Profesional{" +
+                ", run=" + run +
+                ", nombre='" + nombre +
+                ", apellidos='" + apellidos +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", edad=" + edad +
+                "titulo='" + titulo +
+                ", fechaIngreso=" + fechaIngreso +
+                '}';
+    }
+}
+
