cb4e69a1ce00395a39d19b1b4d9e55f98f16042d
Pablov <juan.vasquez@valposystem.com>
2023-07-24 20:06:49 -0400

    Modificacion Usuario.java

--- ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/models/Usuario.java
+++ ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/models/Usuario.java
@@ -15,9 +15,7 @@ public class Usuario {
     //Definición de atributos
     protected int run;
     protected String nombre;
-    protected String apellidos;
-    protected LocalDate fechaNacimiento;
-    protected Integer edad;
+    protected String tipo;
 
     /**
      * Construct vacío
@@ -28,16 +26,12 @@ public class Usuario {
     /**
      * @param run
      * @param nombre
-     * @param apellidos
-     * @param fechaNacimiento
-     * @param edad
+     * @param tipo
      */
-    public Usuario(int run, String nombre, String apellidos, LocalDate fechaNacimiento, Integer edad) {
+    public Usuario(int run, String nombre, String tipo) {
         this.run = run;
         this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.fechaNacimiento = fechaNacimiento;
-        this.edad = edad;
+        this.tipo= tipo;
     }
 
     /**
@@ -68,46 +62,12 @@ public class Usuario {
         this.nombre = nombre;
     }
 
-    /**
-     * @return apellidos
-     */
-    public String getApellidos() {
-        return this.apellidos;
+    public String getTipo() {
+        return tipo;
     }
 
-    /**
-     * @param apellidos
-     */
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    /**
-     * @return fechaNacimiento
-     */
-    public LocalDate getFechaNacimiento() {
-        return this.fechaNacimiento;
-    }
-
-    /**
-     * @param fechaNacimiento
-     */
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    /**
-     * @return edad
-     */
-    public Integer getEdad() {
-        return this.edad;
-    }
-
-    /**
-     * @param edad
-     */
-    public void setEdad(Integer edad) {
-        this.edad = edad;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
     /**
@@ -118,9 +78,7 @@ public class Usuario {
         return "Usuarios{" +
                 "run=" + run +
                 ", nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                ", fechaNacimiento=" + fechaNacimiento +
-                ", edad=" + edad +
+                ", tipo='" + tipo + '\'' +
                 '}';
     }
 
@@ -130,7 +88,7 @@ public class Usuario {
      * @return Un mensaje con la edad del usuario.
      */
     public String mostrarEdad() {
-        return "El usuario tiene " + edad + " años.";
+        return "El usuario es: " + tipo  ;
     }
 
 }
