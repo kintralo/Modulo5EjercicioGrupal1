348349f79846004a7adf59ac5e50ca899ecd01cb
Pablov <juan.vasquez@valposystem.com>
2023-07-24 21:07:15 -0400

    Creación lógica clase UsuarioDao.java

--- ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/dao/UsuarioDao.java
+++ ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/dao/UsuarioDao.java
@@ -1,18 +1,67 @@
 package com.example.ejerciciogrupal1.dao;
 
+import com.example.ejerciciogrupal1.conexion.Conexion;
 import com.example.ejerciciogrupal1.interfaces.IUsuario;
+import com.example.ejerciciogrupal1.models.Capacitacion;
 import com.example.ejerciciogrupal1.models.Usuario;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioDao implements IUsuario {
+
+    private Statement statement = null;
+    private Connection connection= null;
+    private ResultSet resultSet=  null;
     @Override
     public boolean createUsuario(Usuario usuario) throws Exception {
-        return false;
+        boolean registar = false;
+        String sql=" INSERT INTO usuarios (nombre, tipo) VALUES ('" +usuario.getNombre()+
+                usuario.getTipo()+"')";
+        //Creación de try
+        try {
+            connection = Conexion.conectar();//Agregar los datos de la conexión
+            statement = connection.createStatement();
+            statement.execute(sql);//Agregar la consulta para registrar
+            registar = true;//Si salío todo de pana
+            statement.close();// Cerrar declacaració
+            connection.close();// Cerrar conexión-
+        } catch (SQLException e) {
+            System.out.println("Error: createCapacitacion");
+            e.printStackTrace();
+        }
+        return registar;
     }
 
     @Override
     public List<Usuario> listaUsuarios() throws Exception {
-        return null;
+        String sql="SELECT *FROM usuarios";
+        List<Usuario> usuarioList =new ArrayList<Usuario>();
+        //Creación de try
+        try {
+            connection = Conexion.conectar();//Agregar los datos de la conexión
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);//Agregar la consulta para registrar
+
+            while (resultSet.next()) {
+                //Creación de un objeto para agregarlo a la lista
+                Usuario usuario = new Usuario();
+                //Obtencion de datos de la tabla campo por campo
+                usuario.setNombre(resultSet.getString(1));
+                usuario.setTipo(resultSet.getString(2));
+                usuarioList.add(usuario);//Guardar los datos a lista
+            }
+            resultSet.close();// Cerrar resultset
+            statement.close();// Cerrar declacaración
+            connection.close();// Cerrar conexión-
+        } catch (SQLException e) {
+            System.out.println("Error: listaCapacitaciones");
+            e.printStackTrace();
+        }
+        return usuarioList;
     }
 }
