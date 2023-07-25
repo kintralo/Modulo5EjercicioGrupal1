ed0730f567c05e5da903f1b825a757e28465c0e0
Pablov <juan.vasquez@valposystem.com>
2023-07-25 15:05:44 -0400

    Creación de métodos para eliminar y actualizar.

--- ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/implementacion/UsuarioDao.java
+++ ejercicioGrupal1/src/main/java/com/example/ejerciciogrupal1/implementacion/UsuarioDao.java
@@ -10,15 +10,26 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ * @author Juan Pablo Vásquez
+ * Ándres Tapia
+ * Victor Briso
+ * Sebastián Araya
+ * @version 1.0
+ */
+
+/**
+ * Clase  de acceso a datos UsuarioDao.
+ */
 
 public class UsuarioDao implements IUsuario {
-
+    //Varaibles
+    private String sql = null;
     private Statement statement = null;
     private Connection connection = null;
     private ResultSet resultSet = null;
 
     /**
-     *
      * @param usuario
      * @return
      * @throws Exception
@@ -26,7 +37,8 @@ public class UsuarioDao implements IUsuario {
     @Override
     public boolean createUsuario(Usuario usuario) throws Exception {
         boolean registar = false;
-        String sql = " INSERT INTO usuarios (nombre, tipo) VALUES ('" + usuario.getNombre() +
+        //Creamos la consulta SQL  para agrgar nuevos valores en la tabla.
+        sql = " INSERT INTO usuarios (nombre, tipo) VALUES ('" + usuario.getNombre() +
                 "', '" + usuario.getTipo() + "')";
         //Creación de try
         try {
@@ -44,24 +56,41 @@ public class UsuarioDao implements IUsuario {
     }
 
     /**
-     *
      * @param usuario
      * @return
      * @throws Exception
      */
     @Override
     public boolean updateUsuario(Usuario usuario) throws Exception {
-        return false;
+        boolean actualizar = false;
+        //Creamos la consulta SQL  para actualizar los valores en la tabla.
+        sql = "UPDATE  usuarios SET "
+                + ",nombre= '" + usuario.getNombre()
+                + "',tipo='" + usuario.getTipo()
+                + "'WHERE  id=" + usuario.getId_usuario();
+        //Creación de try
+        try {
+            connection = Conexion.conectar();//Agregar los datos de la conexión
+            statement = connection.createStatement();
+            statement.execute(sql);//Agregar la consulta para registrar
+            actualizar = true;//Si salío todo de pana
+            statement.close();// Cerrar declacaració
+            connection.close();// Cerrar conexión-
+        } catch (SQLException e) {
+            System.out.println("Error: updateUsuario");
+            e.printStackTrace();
+        }
+        return actualizar;
     }
 
     /**
-     *
      * @return
      * @throws Exception
      */
     @Override
     public List<Usuario> listaUsuarios() throws Exception {
-        String sql = "SELECT * FROM usuarios";
+        //Creamos la consulta SQL  para todos los valores en la tabla.
+        sql = "SELECT * FROM usuarios";
         List<Usuario> usuarioList = new ArrayList<Usuario>();
         //Creación de try
         try {
@@ -89,13 +118,27 @@ public class UsuarioDao implements IUsuario {
     }
 
     /**
-     *
      * @param usuario
      * @return
      * @throws Exception
      */
     @Override
     public boolean eliminarUsuario(Usuario usuario) throws Exception {
-        return false;
+        boolean eliminar = false;
+        //Creamos la consulta SQL  para eliminra los valores en la tabla.
+        sql = "DELETE FROM  usuarios  WHERE id =" + usuario.getId_usuario();//Eliminamos donde esta el id
+        //Creación de try
+        try {
+            connection = Conexion.conectar();//Agregar los datos de la conexión
+            statement = connection.createStatement();
+            statement.execute(sql);//Agregar la consulta para registrar
+            eliminar = true;//Si salío todo de pana
+            statement.close();// Cerrar declacaració
+            connection.close();// Cerrar conexión-
+        } catch (SQLException e) {
+            System.out.println("Error: updateUsuario");
+            e.printStackTrace();
+        }
+        return eliminar;
     }
 }
