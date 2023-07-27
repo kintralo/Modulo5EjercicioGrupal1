df2de3343e15ca8057a2d4f264f1f6df039aca35
Pablov <juan.vasquez@valposystem.com>
2023-07-26 23:05:14 -0400

    Script módulo5

--- prevencion_riesgos_capacitaciones.sql
+++ prevencion_riesgos_capacitaciones.sql
@@ -1,52 +1,30 @@
-CREATE DATABASE  IF NOT EXISTS `prevencion_riesgos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `prevencion_riesgos`;
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
---
--- Host: localhost    Database: prevencion_riesgos
--- ------------------------------------------------------
--- Server version	8.0.33
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `capacitaciones`
---
-
+CREATE DATABASE prevencion_riesgos;
+USE prevencion_riesgos;
 DROP TABLE IF EXISTS `capacitaciones`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `capacitaciones` (
-  `identificador` int NOT NULL AUTO_INCREMENT,
-  `rut` int DEFAULT NULL,
+CREATE TABLE capacitaciones (
+  `identificador` int(11) NOT NULL AUTO_INCREMENT,
+  `rut` int(11) DEFAULT NULL,
+  `dia` varchar(100) NOT NULL,
+  `hora` time DEFAULT NULL,
+  `lugar` varchar(100) NOT NULL,
+  `duracion` time DEFAULT NULL,
+  `cantAsistentes` int(11) DEFAULT NULL,
   PRIMARY KEY (`identificador`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=1234524 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `capacitaciones`
---
 
 LOCK TABLES `capacitaciones` WRITE;
 /*!40000 ALTER TABLE `capacitaciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `capacitaciones` ENABLE KEYS */;
+INSERT INTO `capacitaciones` VALUES (
+101,1892943,'Lunes','12:00:00',' Segunda prueba','02:00:00',12),
+(102,1892943,'Martes','12:00:00','En el capitolio','02:00:00',12),
+(103,1892943,'Lunes','12:00:00','El cuerpo de Bomberos','02:00:00',12),
+(104,103988,'Jueves','11:30:00',' Cede municipal ','01:00:00',12),
+(105,103988,'Lunes','11:30:00',' 12:12','01:00:00',12),
+(106,103988,'Lunes','12:30:00','Es aqui','01:00:00',7),
+(107,103988,'Lunes','11:30:00','Test 1 ','01:00:00',12),
+(108,103988,'Lunes','11:30:00','test 2','01:00:00',12),
+(1234523,1892943,'Lunes','12:00:00','En la moneda','02:00:00',12);
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-07-26 22:41:11
